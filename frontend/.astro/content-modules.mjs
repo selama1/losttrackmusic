d98f502e58ffa4eb7e4fc1fbaeb4884be5e2d93e
Sela Mador-Haim <selama@gmail.com>
2025-08-09 09:37:53 -0500

    Add vercel analytics

---
+++ frontend/.astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/building-the-machine.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbuilding-the-machine.mdx&astroContentModuleFlag=true")],
+["src/content/blog/palette-brighter.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpalette-brighter.mdx&astroContentModuleFlag=true")]]);
+		
